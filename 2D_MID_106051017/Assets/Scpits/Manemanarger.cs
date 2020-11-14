58a51a6d25446905d644c1da8a30596fb9a8bf34
Elaine0916 <55180609+Elaine0916@users.noreply.github.com>
2020-11-14 22:24:08 +0800

    離開遊戲按鈕6-4

--- 2D_MID_106051017/Assets/Scpits/Manemanarger.cs
+++ 2D_MID_106051017/Assets/Scpits/Manemanarger.cs
@@ -4,12 +4,13 @@ public class Manemanarger : MonoBehaviour
 {
     public void StartGmae ()
     {
-
+        print("開始遊戲");
     } 
 
     public void QuitGmae ()
     {
 
-
+        print("結束遊戲");
+        Application.Quit();
     }
 }
